e2ab12e6c3eaeb9eecf0416a66cc198da5110119
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-10-26 01:27:40 +0600

    server setup

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const port = process.env.PORT || "5000";
+const courses = require('./data/course.json')
 app.get('/', (req , res) => {
     res.send('server started')
+})
+app.get('/courses', (req , res) => {
+    res.send(courses)
+})
+app.get('/courses/:id', (req , res) => {
+    const id = req.params.id;
+    const course = courses.find(co => co.id == id);
+    res.send(course)
 })
 
 app.listen(port, () => {
